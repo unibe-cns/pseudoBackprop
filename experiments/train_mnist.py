08e46f947b22084866f6b9745806a34ea10e36dc
fkungl <fkungl@kip.uni-heidelberg.de>
2020-10-02 19:53:58 +0200

    Make the experiment parametrizable

--- experiments/train_mnist.py
+++ experiments/train_mnist.py
@@ -3,6 +3,8 @@
 """
 import logging
 import os
+import argparse
+import json
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -13,16 +15,23 @@ logging.basicConfig(format='Train MNIST -- %(levelname)s: %(message)s',
                     level=logging.DEBUG)
 
 
-def main():
+def main(params):
     """
         Execute the training and save the result
     """
 
     # parameters of the learning
-    batch_size = 10
-    layers = [784, 700, 10]
-    epochs = 10
-    model_folder = 'models'
+    batch_size = params["batch_size"]
+    layers = params["layers"]
+    epochs = params["epochs"]
+    model_folder = params["model_folder"]
+    model_type = params["model_type"]
+    learning_rate = params["learning_rate"]
+    momentum = params["momentum"]
+
+    # set-up the folder to save the model
+    if not os.path.exists(model_folder):
+        os.makedirs(model_folder)
 
     # set up the normalizer
     # Normalize the images to
@@ -39,12 +48,19 @@ def main():
     logging.info("Datasets are loaded")
 
     # make the networks
-    backprop_net = FullyConnectedNetwork.backprop(layers)
+    possible_networks = ['fa', 'backprop']
+    if model_type == 'fa':
+        backprop_net = FullyConnectedNetwork.feedback_alignement(layers)
+    elif model_folder == 'backprop':
+        backprop_net = FullyConnectedNetwork.backprop(layers)
+    else:
+        raise ValueError(f'{model_type} is not a valid option. Implemented \
+            options are in {possible_networks}')
 
     # set up the optimizer and the loss function
     loss_function = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(
-        backprop_net.parameters(), lr=0.001, momentum=0.9)
+        backprop_net.parameters(), lr=learning_rate, momentum=momentum)
 
     # train the network
     for epoch in range(epochs):  # loop over the dataset multiple times
@@ -68,20 +84,34 @@ def main():
             # print statistics
             # running loss is the loss measured on the last 2000 minibatches
             running_loss += loss_value.item()
-            if index % 2000 == 1999:    # print every 2000 mini-batches
+            if index % (10000/batch_size) == 999:
+                # print every 2000 mini-batches
                 logging.info(f'epoch {epoch}, batch {index}, \
                               loss: {running_loss}')
                 running_loss = 0.0
+                # save the model every 10000 examples
+                file_to_save = (f"model_{model_type}_epoch_{epoch}_images_"
+                                f"{(index + 1) * batch_size}.pth")
+                path_to_save = os.path.join(model_folder, file_to_save)
+                torch.save(backprop_net.state_dict(),
+                           path_to_save)
 
     logging.info('The training has finished')
 
     # save the result
-    logging.info("Saving the backprop model")
-    if not os.path.exists(model_folder):
-        os.makedirs(model_folder)
+    logging.info("Saving the model")
+
     torch.save(backprop_net.state_dict(),
                os.path.join(model_folder, 'mnist_backprop.pth'))
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Train a model on the mnist \
+        dataset.')
+    parser.add_argument('--params', type=str,
+                        help='Path to the parameter json.')
+    args = parser.parse_args()
+    with open(args.params, 'r+') as f:
+        parameters = json.load(f)
+
+    main(parameters)
