b21e1b1251042f7655a7dd3100bede35e3ea7466
fkungl <fkungl@kip.uni-heidelberg.de>
2020-11-13 16:52:07 +0100

    Add calc gen pseudo tests

--- tests/calc_pseudo_test.py
+++ tests/calc_pseudo_test.py
@@ -4,6 +4,7 @@ import torch
 import nose
 import numpy as np
 from pseudo_backprop.network import FullyConnectedNetwork
+import pseudo_backprop.aux as aux
 
 logging.basicConfig(format='Tests -- %(levelname)s: %(message)s',
                     level=logging.DEBUG)
@@ -25,8 +26,7 @@ class TestClassPseudoBackpropagation:
         cls.net.redo_backward_weights()
 
     def calc_pseudo_test_init(self):
-        """ make the network calculate the pseudo-inverse and check if it is
-            true in the beginning
+        """ Calculate the pseudo-inverse in network and check (init)
         """
         logging.info('Running the initial pseudo test.')
 
@@ -49,8 +49,7 @@ class TestClassPseudoBackpropagation:
             nose.tools.assert_greater(frob_rand, frob)
 
     def calc_pseudo_test_train(self):
-        """ make the network calculate the pseudo-inverse and check if it is
-            true after some training """
+        """ Calculate the pseudo-inverse in network and check (trained) """
         logging.info('Running pseudo test after training')
 
         optimizer = torch.optim.SGD(self.net.parameters(),
@@ -101,8 +100,7 @@ class TestClassGenPseudoBackpropagation:
         cls.net.redo_backward_weights(cls.random_input)
 
     def calc_pseudo_test_init(self):
-        """ make the network calculate the pseudo-inverse and check if it is
-            true in the beginning
+        """ Calculate the pseudo-inverse in network and check (init)
         """
         logging.info('Running the initial pseudo test.')
 
@@ -121,12 +119,10 @@ class TestClassGenPseudoBackpropagation:
             in zip(forward_weights, backward_weights)]
         print(frob_norms)
         print(frob_norm_rand)
-        # for (frob, frob_rand) in zip(frob_norms, frob_norm_rand):
-        #    nose.tools.assert_greater(frob_rand, frob)
+        self._check_gen_pseudo()
 
     def calc_pseudo_test_train(self):
-        """ make the network calculate the pseudo-inverse and check if it is
-            true after some training """
+        """ Calculate the pseudo-inverse in network and check (trained) """
         logging.info('Running pseudo test after training')
 
         optimizer = torch.optim.SGD(self.net.parameters(),
@@ -142,25 +138,48 @@ class TestClassGenPseudoBackpropagation:
             outputs = self.net(self.random_input)
             loss_value = loss_function(outputs, self.random_output)
             loss_value.backward()
+            print(loss_value)
             optimizer.step()
+
+        self.net.redo_backward_weights(self.random_input)
+        self._check_gen_pseudo()
+
+    def _check_gen_pseudo(self):
+        """Go over the synapses and check them for the gen pseudoinverse
+        """
 
         forward_weights = self.net.get_forward_weights()
         backward_weights = self.net.get_backward_weights()
-        frob_norms_before = [np.linalg.norm(np.linalg.pinv(w)-b, ord='fro')
-                             for (w, b)
-                             in zip(forward_weights, backward_weights)]
-        self.net.redo_backward_weights(self.random_input)
-        backward_weights = self.net.get_backward_weights()
-        frob_norms_after = [np.linalg.norm(
-            np.linalg.pinv(w)-b,
-            ord='fro') for (w, b)
-            in zip(forward_weights, backward_weights)]
-        print(frob_norms_before)
-        print(frob_norms_after)
-        # for (frob_bef, frob_af) in zip(frob_norms_before, frob_norms_after):
-        #    nose.tools.assert_greater(frob_bef, frob_af)
 
-
-# testClass = TestClassGenPseudoBackpropagation()
-# testClass.setup_class()
-# testClass.calc_pseudo_test_init()
+        for index in range(len(self.layers) - 1):
+            if self.layers[index] <= self.layers[index + 1]:
+                logging.info('Testing against pseudoinverse')
+                diff = np.linalg.pinv(
+                    forward_weights[index]) - backward_weights[index]
+                frob_norm = np.linalg.norm(diff, ord='fro')
+                frob_rand = np.linalg.norm(
+                    backward_weights[index] -
+                    np.random.rand(*backward_weights[index].shape), ord='fro')
+                logging.info(f'Frob norm: B to random {frob_rand}')
+                logging.info(f'Frob norm: B to W-pseudo {frob_norm}')
+                nose.tools.assert_greater(frob_rand, frob_norm)
+            else:
+                logging.info('Testing against energy')
+                w_matrix = forward_weights[index]
+                b_matrix = backward_weights[index]
+                input_data = self.net.forward_to_hidden(self.random_input,
+                                                        index)
+                loss_b = aux.calc_loss(b_matrix,
+                                       w_matrix,
+                                       input_data.detach().numpy().T)
+                loss_pinv = aux.calc_loss(np.linalg.pinv(w_matrix),
+                                          w_matrix,
+                                          input_data.detach().numpy().T)
+                loss_random = aux.calc_loss(np.random.rand(*b_matrix.shape),
+                                            w_matrix,
+                                            input_data.detach().numpy().T)
+                logging.info(f"Loss with gen pseudo: {loss_b}")
+                logging.info(f"Loss with W pinv: {loss_pinv}")
+                logging.info(f"Loss with random matrix: {loss_random}")
+                nose.tools.assert_greater(loss_random, loss_pinv)
+                nose.tools.assert_greater(loss_pinv, loss_b)
