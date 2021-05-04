Fixed calculation of data-specific pseudoinverse. Now uses autocorrelation (instead of covariance) matrix. Fixed log output for example. Added functionality to measure cosine similarity between tensors. Added function to get the data-specific pseudoinverse.

pseudo_backprop/aux.py
@@ -60,8 +60,11 @@ def generalized_pseudo(w_matrix, dataset):
 
     np_dataset = dataset.detach().cpu().numpy()
     covariance = np.cov(np_dataset.T)
+    mean = np.mean(np_dataset, axis=0)
+    gammasquared = covariance + np.outer(mean,mean)
+    
     # make the singular value decomposition
-    u_matrix, s_matrix, vh_matrix = np.linalg.svd(covariance)
+    u_matrix, s_matrix, vh_matrix = np.linalg.svd(gammasquared)
     # Calculate the generalized pseudoinverse
     gamma = np.dot(np.dot(u_matrix, np.diag(np.sqrt(s_matrix))), vh_matrix)
     gen_pseudo = np.dot(gamma, np.linalg.pinv(np.dot(w_matrix, gamma)))

pseudo_backprop/experiments/test_mnist.py
@@ -7,6 +7,7 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 from pseudo_backprop.aux import evaluate_model
+from pseudo_backprop.aux import generalized_pseudo
 from pseudo_backprop.experiments import exp_aux
 from pseudo_backprop.experiments.yinyang_dataset.dataset import YinYangDataset
 
@@ -89,7 +90,7 @@ def main(params, dataset, per_images=10000):
             * per_images
         file_to_load = (f"model_{model_type}_epoch_{epoch}_images_"
                         f"{ims}.pth")
-        logging.info(f'Working on epoch {epoch} and image {ims}.')
+        logging.info(f'• Working on epoch {epoch} and image {ims}.')
         path_to_model = os.path.join(model_folder, file_to_load)
         backprop_net.load_state_dict(torch.load(path_to_model))
         # Evaluate the model