Added function covariance in torch. Speed improvements when calculating Gamma squared. Using Gamma squared to calculate mismatch energy (instead of Gamma). Fixed bug where Gamma was calculated only at init, now at every calculation of mismatch energy.

pseudo_backprop/experiments/train_mnist.py
@@ -10,7 +10,7 @@ import numpy as np
 from tqdm import tqdm
 from pseudo_backprop.experiments import exp_aux
 from pseudo_backprop.experiments.yinyang_dataset.dataset import YinYangDataset
-from pseudo_backprop.aux import calc_mismatch_energy
+from pseudo_backprop.aux import *
 
 torch.autograd.set_detect_anomaly(True)
 logging.basicConfig(format='Train model -- %(levelname)s: %(message)s',
@@ -114,7 +114,7 @@ def main(params):
                 trainset,
                 batch_size=len(trainset))
             if PRINT_DEBUG: timer = time.time()
-            data = next(iter(data_samp))[0].view(len(trainset), -1)
+            input_data = next(iter(data_samp))[0].view(len(trainset), -1)
             if PRINT_DEBUG: logging.info(f'Time to load data: {time.time()-timer}s')
             # calling the second dataloader changes the RNG state, so we reset
             torch.manual_seed(random_seed)
@@ -158,11 +158,12 @@ def main(params):
     # for dyn pseudo, calculate the matrix Gamma
     # (sqrt of the autocorrelation) to calculate mismatch energy
     if model_type == 'dyn_pseudo':
-        if PRINT_DEBUG: gamma_timer = time.time()
-        Gamma_array = backprop_net.get_gamma_matrix(dataset=data)
-        if PRINT_DEBUG: logging.info(f'Time to calculate Gamma matrices: {time.time()-gamma_timer}s')
+        # if PRINT_DEBUG: gamma_timer = time.time()
+        # Gamma_array = backprop_net.get_gamma_matrix(dataset=input_data)
+        # if PRINT_DEBUG: logging.info(f'Time to calculate Gamma matrices: {time.time()-gamma_timer}s')
         # initialise an array to save the mismatch energies
         mm_energy = []
+        # mm_energy_fast = []
         # count how often mismatch energy in each layer has been calculated
         # since last update of regularizer
         mm_energy_counter = [0 for layer in range(len(layers)-1)]
@@ -258,17 +259,25 @@ def main(params):
                     # calculate mismatch energy
                     W_array = backprop_net.get_forward_weights()
                     B_array = backprop_net.get_backward_weights()
+
+                    # if PRINT_DEBUG: gamma_timer = time.time()
+                    # Gamma_array = backprop_net.get_gamma_matrix(dataset=input_data)
+                    # if PRINT_DEBUG: logging.info(f'Time to calculate Gamma matrices: {time.time()-gamma_timer}s')
+
+                    if PRINT_DEBUG: gamma_timer = time.time()
+                    Gamma2_array = backprop_net.get_gamma2_matrix(dataset=input_data)
+                    if PRINT_DEBUG: logging.info(f'Time to calculate squared Gamma matrices: {time.time()-gamma_timer}s')
                     
                     if PRINT_DEBUG: timer = time.time()
                     mm_energy.append(
-                            [ calc_mismatch_energy(
-                                Gamma_array[i].numpy(), B_array[i].T, W_array[i], regularizer_array[i]
+                            [ calc_mismatch_energy_fast(
+                                Gamma2_array[i].numpy(), B_array[i].T, W_array[i], regularizer_array[i]
                                 )
                               for i in range(len(backprop_net.synapses))]
                         )
                     if PRINT_DEBUG: logging.info(f'Time to calculate mismatch energy: {time.time()-timer}s')
+                    
                     mm_energy_counter = [x + 1 for x in mm_energy_counter]
-                    # print(mm_energy)
 
                     for i in range(len(backprop_net.synapses)):
                         logging.info(f'Mismatch energy in layer {i}: {mm_energy[-1][i]}')